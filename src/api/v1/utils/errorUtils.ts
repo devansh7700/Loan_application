1a9c2f971b400424b40adf500a09e47a5a9fc766
Devanshgarg <dgarg3@academic.rrc.ca>
2025-11-03 23:49:28 -0600

    Created utility function for fetching error information

---
+++ src/api/v1/utils/errorUtils.ts
@@ -0,0 +1,18 @@
+import { AppError } from "../errors/Apperror";
+
+export const formatError = (err: unknown) => {
+  if (err instanceof AppError) {
+    return {
+      message: err.message,
+      statusCode: err.statusCode,
+      timestamp: new Date().toISOString(),
+    };
+  }
+
+  // fallback for unknown errors
+  return {
+    message: "Internal server error",
+    statusCode: 500,
+    timestamp: new Date().toISOString(),
+  };
+};
