e973e9d7c7aefe8037675f79a457419130c176d9
Devanshgarg <dgarg3@academic.rrc.ca>
2025-11-04 01:23:17 -0600

    implemented the logger in the main file

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,13 @@ import express from "express";
 import morgan from "morgan";
 import loanRoutes from "./api/v1/routes/loanRoutes";
 import { errorHandler } from "./api/v1/middleware/errorHandler";
-
+import { loggerMiddleware } from "./api/v1/middleware/loggerMiddleware";
 const app = express();
 
 // Middleware
 app.use(express.json());
 app.use(morgan("dev"));
+app.use(loggerMiddleware);
 
 // Routes
 app.use("/api/v1", loanRoutes);
